8fd861c4716c1003aff3c1b4bf3738a86de4a73c
Vladislav Kozikovskiy <ultratapok@gmail.com>
2024-09-07 14:55:15 +0300

    readme 1.05

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(id_state_date_list: list[dict], state: str='EXECUTED') -> list[dict]:
+def filter_by_state(id_state_date_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает список словарей и опционально значение для ключа state.
     Возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению."""
@@ -9,8 +9,8 @@ def filter_by_state(id_state_date_list: list[dict], state: str='EXECUTED') -> li
     return filtered_by_state_list
 
 
-def sort_by_date(id_state_date_list: list[dict], sort_is_reversed: bool=True) -> list[dict]:
+def sort_by_date(id_state_date_list: list[dict], sort_is_reversed: bool = True) -> list[dict]:
     """Функция принимает список словарей и необязательный параметр порядока сортировки (по умолчанию — убывание).
     Возвращает новый список, отсортированный по дате"""
-    sorted_by_date_list = sorted(id_state_date_list, key=lambda x: x['date'], reverse = sort_is_reversed)
+    sorted_by_date_list = sorted(id_state_date_list, key=lambda x: x["date"], reverse=sort_is_reversed)
     return sorted_by_date_list
