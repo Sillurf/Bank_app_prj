96ddd86239f413ae65eaa55d544904caaebb9347
Vladislav Kozikovskiy <ultratapok@gmail.com>
2024-09-10 01:44:58 +0300

    test_widget.py

--- src/widget.py
+++ src/widget.py
@@ -3,16 +3,22 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_num: str) -> str:
     """Функция принимает номер карты или счёта и выводит их с соответствующей маской."""
-    card_or_account_num_list = card_or_account_num.split()
-
-    if "Счет" in card_or_account_num:
-        masked_account = "Счет " + get_mask_account(card_or_account_num_list[-1])
-        return masked_account
+    if card_or_account_num:
+        card_or_account_num_list = card_or_account_num.split()
+        if "Счет" in card_or_account_num_list:
+            if get_mask_account(card_or_account_num_list[-1]) == "Некорректное значение номера счёта":
+                return "Некорректное значение номера счёта"
+            masked_account = "Счет " + get_mask_account(card_or_account_num_list[-1])
+            return masked_account
+        else:
+            if get_mask_card_number(card_or_account_num_list[-1]) == "Некорректное значение номера карты":
+                return "Некорректное значение номера карты или счёта"
+            card_num = card_or_account_num_list[-1]
+            card_or_account_num_list[-1] = get_mask_card_number(card_num)
+            masked_card_number = " ".join(card_or_account_num_list)
+            return masked_card_number
     else:
-        card_num = card_or_account_num_list[-1]
-        card_or_account_num_list[-1] = get_mask_card_number(card_num)
-        masked_card_number = " ".join(card_or_account_num_list)
-        return masked_card_number
+        return "Некорректное значение"
 
 
 def get_date(date_in: str) -> str:
@@ -20,5 +26,17 @@ def get_date(date_in: str) -> str:
     "2024-03-11T02:26:18.671407"
      и возвращает строку с датой в формате
     "11.03.2024"."""
-    date_out = date_in[8:10] + "." + date_in[5:7] + "." + date_in[0:4]
-    return date_out
+    if not date_in or len(date_in) < 10:
+        return "Некорректный формат даты"
+    else:
+        date_for_check = date_in[8:10] + date_in[5:7] + date_in[0:4]
+        if (
+            date_for_check.isdigit()
+            and 1 <= int(date_in[8:10]) <= 31
+            and 1 <= int(date_in[5:7]) <= 12
+            and int(date_in[0:4])
+        ):
+            date_out = date_in[8:10] + "." + date_in[5:7] + "." + date_in[0:4]
+            return date_out
+        else:
+            return "Некорректный формат даты"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,4 +1,29 @@
 from src.widget import mask_account_card, get_date
+import pytest
 
-@pytest.parametrize ()
-def
+
+@pytest.mark.parametrize(
+    "card_or_account_num, expected",
+    [
+        ("Счет 73654108430135874305", "Счет **4305"),                            # Стандартный счет
+        ("Фирма карты 1234123412341234", "Фирма карты 1234 12** **** 1234"),     # Стандартная карта
+        ("", "Некорректное значение"),                                           # Пустая строка
+        ("Счет  Иванова", "Некорректное значение номера счёта")                  # Случайное значение номера счета
+    ]
+)
+def test_mask_account_card(card_or_account_num, expected):
+    assert mask_account_card(card_or_account_num) == expected
+
+@pytest.mark.parametrize(
+    "date_in, expected",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),                 # Стандартный ввод
+        ("", "Некорректный формат даты"),                             # Пустая строка
+        ("2023.12.12", "12.12.2023"),                                 # Дата без времени и не через дефис
+        ("2021-13-01T02:26:18.671407", "Некорректный формат даты"),   # 13й месяц
+        ("2021-10-32T02:26:18.671407", "Некорректный формат даты"),   # 32ой день
+        ("sdasdsadsdsadsad", "Некорректный формат даты")              # Случайная строка
+    ]
+)
+def test_get_date(date_in, expected):
+    assert get_date(date_in) == expected
